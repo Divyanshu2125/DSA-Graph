1cfb5ca700c908517463b0e9c4d2b80c13520abc
Divyanshu Kaushal <123375624+Divyanshu2125@users.noreply.github.com>
2024-04-18 22:32:41 +0530

    Update Adjancy list.cpp

--- Adjancy list.cpp
+++ Adjancy list.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 class Graph{
   public:
-  unordered_map<int,list<int> > adjlist;
+  unordered_map<int,list<pair <int,int>> > adjlist;
 
-  void addedge(int u,int v,bool direction){
+  void addedge(int u,int v,int weight,bool direction){
     //direction 0 -> undirected edge
     //direction 1 -> directed edge
 
-    adjlist[u].push_back(v);
+    adjlist[u].push_back({v,weight});
     if(direction==0){
-      adjlist[v].push_back(u);
+      adjlist[v].push_back({u,weight});
     }
   }
 
@@ -23,7 +23,7 @@ class Graph{
     for(auto node :adjlist){
       cout<<node.first<<"->";
       for(auto neighbour : node.second){
-        cout<<neighbour<< " ";
+        cout<<"("<<neighbour.first<< ","<<neighbour.second<<")";
       }
       cout<<endl;
     }
@@ -32,9 +32,9 @@ class Graph{
 
 int main() {
   Graph g;
-  g.addedge(0,1,0);
-  g.addedge(1,2,0);
-  g.addedge(0,2,0);
+  g.addedge(0,1,5,0);
+  g.addedge(1,2,6,0);
+  g.addedge(0,2,7,0);
   cout<<endl;
   g.printadjacencylist();
   return 0;
